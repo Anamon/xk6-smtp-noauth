e7e3797df9dca234ce81024fd75842d2ab2ba3ef
Daniel Saner <DanielMarco.Saner@swisscom.com>
2025-10-30 09:31:45 +0100

    Add support for attachments

--- smtp.go
+++ smtp.go
@@ -1,8 +1,11 @@
 package smtp
 
 import (
+	"encoding/base64"
 	"fmt"
 	"net/smtp"
+	"os"
+	"strings"
 
 	"go.k6.io/k6/js/modules"
 )
@@ -14,9 +17,10 @@ func init() {
 type SMTP struct{}
 
 type options struct {
-	Subject string   `js:"subject"`
-	Message string   `js:"message"`
-	UDW     []string `js:"udw"`
+	Subject string `js:"subject"`
+	Message string `js:"message"`
+	UDW []string `js:"udw"`
+	Attachment string `js:"attachment"`
 }
 
 func check(e error) {
@@ -29,12 +33,33 @@ func (*SMTP) SendMail(host string, port string, sender string, recipient string,
 	emailMessage := "From: " + sender + "\r\n" + "To: " + recipient + "\r\n"
 
 	if options.Subject != "" {
-		emailMessage += "Subject: " + options.Subject + "\r\n\r\n"
+		emailMessage += "Subject: " + options.Subject + "\r\n"
 	}
 
-	if options.Message != "" {
-		emailMessage += options.Message
+	boundary := "BOUNDARY12345"
+	emailMessage += "MIME-Version: 1.0\r\n"
+	emailMessage += "Content-Type: multipart/mixed; boundary=" + boundary + "\r\n\r\n"
+
+	emailMessage += "--" + boundary + "\r\n"
+	emailMessage += "Content-Type: text/plain; charset=\"utf-8\"\r\n\r\n"
+	emailMessage += options.Message + "\r\n\r\n"
+
+	if options.Attachment != "" {
+		fileData, err := os.ReadFile(options.Attachment)
+		check(err)
+
+		emailMessage += "--" + boundary + "\r\n"
+		emailMessage += "Content-Type: application/octet-stream\r\n"
+		emailMessage += "Content-Transfer-Encoding: base64\r\n"
+		emailMessage += fmt.Sprintf("Content-Disposition: attachment; filename=\"%s\"\r\n\r\n",
+			getFileName(options.Attachment))
+
+		b64 := make([]byte, base64.StdEncoding.EncodedLen(len(fileData)))
+		base64.StdEncoding.Encode(b64, fileData)
+		emailMessage += string(b64) + "\r\n\r\n"
 	}
+
+	emailMessage += "--" + boundary + "--"
 
 	if len(options.UDW) == 0 {
 		options.UDW = []string{recipient}
@@ -43,4 +68,9 @@ func (*SMTP) SendMail(host string, port string, sender string, recipient string,
 	body := []byte(emailMessage)
 	err := smtp.SendMail(host+":"+port, nil, sender, options.UDW, body)
 	check(err)
+}
+
+func getFileName(path string) string {
+	parts := strings.Split(path, string(os.PathSeparator))
+	return parts[len(parts)-1]
 }
